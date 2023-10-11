142b0c7c3971c507ab18490f5fc2af9751018831
coder-txane <92263760+coder-txane@users.noreply.github.com>
2023-10-11 12:34:48 +0100

    further simplified the rating buttons

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// step 2: make the submit button transition to the thank you page by adding/removing classlists possible
-// step 3: Use innerhtml and template literals to update the result message
-
 let number = document.querySelectorAll('.number-item');
 const submitButton = document.querySelector('.submit-btn');
 const ratingPage = document.querySelector('.rating-container');
@@ -9,29 +6,13 @@ let ratingResult = document.querySelector('.rating-result');
 let rating = 0;
 
 // Rating Buttons
-number.forEach((num) => {
-    let numberValue = num.innerHTML
-    
+number.forEach(num => {
     num.addEventListener("click", () => {
         rating = num.innerHTML;
-        /*
-        if (numberValue === "1") {
-            rating = 1;
-        } else if (numberValue === "2") {
-            rating = 2;
-        } else if (numberValue === "3") {
-            rating = 3;
-        } else if (numberValue === "4") {
-            rating = 4;
-        } else if (numberValue === "5") {
-            rating = 5;
-        }
-        */
     });
-
 });
 
-// Submit button to thank you page
+// Submit button takes you to the thank you page
 submitButton.addEventListener("click", () => {
     if (ratingPage.style.display !== 'none') {
         ratingPage.style.display = 'none';
@@ -39,9 +20,3 @@ submitButton.addEventListener("click", () => {
         ratingResult.innerHTML = `You selected ${rating} out of 5`;
     }
 });
-
-/*
-if (numberRating === "1") {
-    rating = 1;
-}
-*/
